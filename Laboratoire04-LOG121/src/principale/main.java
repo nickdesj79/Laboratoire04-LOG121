9d0b6b5ea6d10bc7b1de9bba0246e942491544cb
nicolas <nicolas@Nick>
2016-03-22 10:35:28 -0400

    initial commit

---
+++ Laboratoire04-LOG121/src/principale/main.java
@@ -0,0 +1,16 @@
+package principale;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test de git hub");
+
+	}
+
+}
